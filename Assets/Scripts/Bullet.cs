e03193b0b240f59d5719ba42a2eb1bd30aecba92
maximelore <maxime.lore@gmail.com>
2016-09-12 15:03:04 +0200

    Bullets maxDist + destroy on contact

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,20 +5,31 @@ public class Bullet : MonoBehaviour {
 
     public float speed;
     public float damage;
+    public float maxDistance;
 
     private Vector3 direction;
+    private Vector3 initialPos;
     private Transform _transform;
 
 	void Start () {
         _transform = GetComponent<Transform>();
+        initialPos = _transform.position;
     }
 
     void Update() {
         _transform.position += direction * speed * Time.deltaTime;
+
+        if (Vector3.Distance(initialPos, _transform.position) >= maxDistance) {
+            Destroy(gameObject);
+        }
     }
 
     public void Init(float angle) {
         angle *= Mathf.Deg2Rad;
         direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+    }
+
+    void OnTriggerEnter2D() {
+        Destroy(gameObject);
     }
 }
