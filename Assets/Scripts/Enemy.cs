c41880effbee6308f94ce79ac506139df0edc741
maximelore <maxime.lore@gmail.com>
2016-09-12 15:03:32 +0200

    enemy life

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,37 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public float meleeDamage;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float life;
+    public float hitFeedbakcDuration;
+
+    private SpriteRenderer spriteRender;
+
+    void Start() {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            life -= other.GetComponent<Player>().meleeDamage;
+        }
+        else if (other.tag == "PlayerBullet") {
+            life -= other.GetComponent<Bullet>().damage;
+        }
+
+        StartCoroutine(hitFeedback());
+    }
+
+    IEnumerator hitFeedback() {
+        spriteRender.color = Color.red;
+        yield return new WaitForSeconds(hitFeedbakcDuration);
+        spriteRender.color = Color.white;
+    }
+
+    void Update() {
+        if(life <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    
 }
