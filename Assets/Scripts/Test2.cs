1bbf7fcf357e9c994cbc2f45fae7c7d6c3a7391c
maximelore <maxime.lore@gmail.com>
2016-09-26 23:52:44 +0200

    misc

---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
