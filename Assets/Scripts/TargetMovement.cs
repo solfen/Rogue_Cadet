5939e6a3e311ec76f17bdcebdc152387656799b8
maximelore <maxime.lore@gmail.com>
2016-09-26 19:15:51 +0200

    TargetMovement stops at wall and face player

--- Assets/Scripts/TargetMovement.cs
+++ Assets/Scripts/TargetMovement.cs
@@ -10,12 +10,18 @@ public class TargetMovement : BaseMovement {
     private Transform player;
     private Vector3 currentDir;
     private Vector3 intialDir;
+    private float initalSpeed = 0;
 	
     void OnEnable() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
         currentDir = player.position - transform.position;
         currentDir = Quaternion.Euler(0, 0, angleOffset) * currentDir;
+
+        if(initalSpeed == 0) {
+            initalSpeed = speed;
+        }
+        speed = initalSpeed;
     }
 
 	void Update () {
@@ -28,12 +34,16 @@ public class TargetMovement : BaseMovement {
             currentDir = Quaternion.Euler(0, 0, angleOffset) * currentDir;
         }
 
+        _transform.up = -(player.position - _transform.position); // "hack" to make it face the player
         _transform.position += currentDir.normalized * speed * Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Wall") {
-            _transform.position -= currentDir.normalized * speed * Time.deltaTime * 10;
+            _transform.position -= currentDir.normalized * speed * Time.deltaTime * 5;
+            if(!updateDir) {
+                speed = 0;
+            }
         }
     }
 
