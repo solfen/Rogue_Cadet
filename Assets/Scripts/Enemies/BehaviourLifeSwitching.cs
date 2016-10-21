55b348b04e1da1c91ac5b65902d8bb0ce04e4959
maximelore <maxime.lore@gmail.com>
2016-10-21 13:18:07 +0200

    BehaviourLifeSwitching

---
+++ Assets/Scripts/Enemies/BehaviourLifeSwitching.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class BehaviourLifeSwitch {
+    public Behaviour script;
+    [Tooltip("inclusive")]
+    [Range(0,1)]
+    public float minLifePercent;
+    [Tooltip("exclusive")]
+    [Range(0,1)]
+    public float maxLifePercent;
+    //[HideInInspector]
+    public float minLife;
+    //[HideInInspector]
+    public float maxLife;
+}
+
+[RequireComponent(typeof(Enemy))]
+public class BehaviourLifeSwitching : MonoBehaviour {
+
+    public List<BehaviourLifeSwitch> behavioursToSwitch;
+    private Enemy bearer;
+	// Use this for initialization
+	void Start () {
+        bearer = GetComponent<Enemy>();
+        for (int i = 0; i < behavioursToSwitch.Count; i++) {
+            behavioursToSwitch[i].minLife = bearer.life * behavioursToSwitch[i].minLifePercent;
+            behavioursToSwitch[i].maxLife = bearer.life * behavioursToSwitch[i].maxLifePercent;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    for(int i = 0; i < behavioursToSwitch.Count; i++) {
+            behavioursToSwitch[i].script.enabled = bearer.life >= behavioursToSwitch[i].minLife && bearer.life < behavioursToSwitch[i].maxLife;
+        }
+	}
+}
