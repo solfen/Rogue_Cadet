a9349b41d9706f0152027dba24fc28b90f5ff5f4
maximelore <maxime.lore@gmail.com>
2016-11-02 12:07:33 +0100

    Enemy ghost

---
+++ Assets/Scripts/Enemies/LifeTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTime : MonoBehaviour, ISwitchable {
+
+    public float lifeTime;
+    private float timer = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= lifeTime) {
+            Destroy(gameObject);
+        }
+	}
+
+    public void SwitchState(bool state) {
+        enabled = state;
+    }
+}
