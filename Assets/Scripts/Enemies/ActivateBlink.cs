43b0f23ef61fb862b2b71e0351ee4d60d33ecb05
maximelore <maxime.lore@gmail.com>
2016-11-02 15:43:03 +0100

    Enemy shield

---
+++ Assets/Scripts/Enemies/ActivateBlink.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateBlink : MonoBehaviour {
+
+    public GameObject target;
+    public float activeDuration;
+    public float inactiveDuration;
+
+    private float timer = 0;
+
+    void Start() {
+        timer = target.activeSelf ? activeDuration : inactiveDuration;
+    }
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+	    if(timer < 0) {
+            target.SetActive(!target.activeSelf);
+            timer = target.activeSelf ? activeDuration : inactiveDuration;
+        }
+	}
+}
