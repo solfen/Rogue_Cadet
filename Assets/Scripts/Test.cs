559901495218208c04a24a8c3781d9f0316f617c
maximelore <maxime.lore@gmail.com>
2016-11-08 18:01:12 +0100

    Ohter zones room sand enemies

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Test : MonoBehaviour {
+
+    public Material newMat;
+    public List<GameObject> objs;
+
+	// Use this for initialization
+	void Start () {
+        enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < objs.Count; i++) {
+            Transform tiled = objs[i].transform.GetChild(0);
+            MeshRenderer[] renderers = tiled.GetComponentsInChildren<MeshRenderer>();
+            for(int j = 0; j < renderers.Length; j++) {
+                renderers[j].material = newMat;
+            }
+        }
+
+        enabled = false;
+        Debug.Log("Mat changed");
+	}
+}
