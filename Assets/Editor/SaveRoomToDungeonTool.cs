6049528b97741f35c0b87d1e74a6a8af5efef01f
maximelore <maxime.lore@gmail.com>
2016-11-09 13:41:51 +0100

    save multiple rooms to dungeon

--- Assets/Editor/SaveRoomToDungeonTool.cs
+++ Assets/Editor/SaveRoomToDungeonTool.cs
@@ -11,19 +11,39 @@ public class SaveRoomToDungeonTool {
     }
 
     [MenuItem("Tools/Save room to dungeon _F7")]
-    private static void AddRoomToDungeon() {
-        Room newRoom = Selection.activeGameObject.GetComponent<Room>();
+    private static void AddRoomsToDungeon() {
         Dungeon dungeon = GameObject.FindObjectOfType<Dungeon>();
         if (dungeon == null) {
             Debug.LogError("No Dungeon generator found");
             return;
         }
 
-        if (newRoom.exits.Count < 1) {
-            Debug.LogError("The room doesn't have any exit!");
-            return;
+        for (int i = 0; i < Selection.gameObjects.Length; i++) {
+            Room newRoom = Selection.gameObjects[i].GetComponent<Room>();
+            if (newRoom.exits.Count < 1) {
+                Debug.LogError("The room:" + newRoom.name + " doesn't have any exit!");
+                continue;
+            }
+            else if(newRoom.exits.Count > (newRoom.size.x*2 + newRoom.size.y*2)) {
+                Debug.LogError("The room:" + newRoom.name + " has too many exits!");
+                continue;
+            }
+
+            RoomToDungeon(newRoom, dungeon);
+        }
+    }
+
+    private static bool CompareRoomsExits(Room room, Room model) {
+        for (int j = 0; j < room.exits.Count; j++) {
+            if (room.exits[j].dir != model.exits[j].dir || room.exits[j].pos != model.exits[j].pos) { //implies that exits are stored in the same order
+                return false;
+            }
         }
 
+        return true;
+    }
+
+    private static void RoomToDungeon(Room newRoom, Dungeon dungeon) {
         newRoom.debug = false;
 
         Undo.RecordObject(dungeon, "Add room to dungeon");
@@ -52,15 +72,5 @@ public class SaveRoomToDungeonTool {
         roomConfigs.Add(new RoomList());
         roomConfigs[roomConfigs.Count - 1].rooms.Add(newRoom);
         Debug.Log("Room added. Zone: " + newRoom.zoneIndex + " Room config: " + (roomConfigs.Count - 1));
-    }
-
-    private static bool CompareRoomsExits(Room room, Room model) {
-        for (int j = 0; j < room.exits.Count; j++) {
-            if (room.exits[j].dir != model.exits[j].dir || room.exits[j].pos != model.exits[j].pos) { //implies that exits are stored in the same order
-                return false;
-            }
-        }
-
-        return true;
     }
 }
