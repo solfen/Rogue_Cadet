d10b74654f5b6f2cd596cca5d13957e812798cf8
maximelore <maxime.lore@gmail.com>
2016-11-11 18:41:03 +0100

    Update script opti + rooms modfis

--- Assets/Editor/EnemyUpdateInRooms.cs
+++ Assets/Editor/EnemyUpdateInRooms.cs
@@ -5,45 +5,47 @@ using UnityEditor;
 
 public class EnemyUpdateInRooms {
 
-    private static string[] roomsPaths = { "Assets/Prefabs/Rooms", "Assets/Prefabs/Rooms/Zone1", "Assets/Prefabs/Rooms/Zone2", "Assets/Prefabs/Rooms/Zone3" };
+    private static string[] roomsPaths = { "Assets/Prefabs/Rooms/Test" }; //{ "Assets/Prefabs/Rooms", "Assets/Prefabs/Rooms/Zone1", "Assets/Prefabs/Rooms/Zone2", "Assets/Prefabs/Rooms/Zone3" };
 
     [MenuItem("Tools/Update enemy in rooms _F6")]
     private static void UpdateEnemiesRoom() {
+        List<GameObject> rooms = new List<GameObject>();
+        string[] guids = AssetDatabase.FindAssets("t:GameObject", roomsPaths);
 
-        for (int i = 0; i < Selection.gameObjects.Length; i++) {
-            string regex = "\\b" + Selection.gameObjects[i].name + "\\b|\\b" + Selection.gameObjects[i].name + "(Clone)" + "\\b"; //regex to match for Enemy name whole word or whole word + clone
-            List<GameObject> rooms = new List<GameObject>();
-            string[] guids = AssetDatabase.FindAssets("t:GameObject", roomsPaths);
+        foreach (string guid in guids) {
+            rooms.Add(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(guid)));
+        }
 
-            foreach (string guid in guids) {
-                rooms.Add(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(guid)));
-            }
+        if (rooms.Count < 1) {
+            Debug.LogError("No rooms at " + roomsPaths[0] + ". Make sure it's the right path");
+            return;
+        }
 
-            if (rooms.Count < 1) {
-                Debug.LogError("No rooms at " + roomsPaths[0] + ". Make sure it's the right path");
-                return;
-            }
+        int totalEnemies = 0;
+        for (int i = 0; i < rooms.Count; i++) {
+            GameObject roomGameObject = Object.Instantiate(rooms[i]);
+            roomGameObject.name = rooms[i].name;
+            Enemy[] enemies = roomGameObject.GetComponent<Room>().enemiesParent.GetComponentsInChildren<Enemy>(true);
+            int enemiesRoomUpdated = 0;
 
-            int cpt = 0;
-            for (int j = 0; j < rooms.Count; j++) {
-                GameObject roomGameObject = Object.Instantiate(rooms[j]);
-                roomGameObject.name = rooms[j].name;
-                Enemy[] enemies = roomGameObject.GetComponent<Room>().enemiesParent.GetComponentsInChildren<Enemy>(true);
+            for (int j = 0; j < Selection.gameObjects.Length; j++) {
+                string regex = "\\b" + Selection.gameObjects[j].name + "\\b|\\b" + Selection.gameObjects[j].name + "(Clone)" + "\\b"; //regex to match for Enemy name whole word or whole word + clone
                 for (int k = enemies.Length - 1; k >= 0; k--) {
-                    if (Regex.IsMatch(enemies[k].name, regex)) {
-                        Object.Instantiate(Selection.gameObjects[i], enemies[k].transform.position, enemies[k].transform.rotation, enemies[k].transform.parent);
+                    if (enemies[k] != null && Regex.IsMatch(enemies[k].name, regex)) {
+                        Object.Instantiate(Selection.gameObjects[j], enemies[k].transform.position, enemies[k].transform.rotation, enemies[k].transform.parent);
                         Object.DestroyImmediate(enemies[k].gameObject, true);
-                        cpt++;
+                        totalEnemies++;
+                        enemiesRoomUpdated++;
                     }
                 }
-
-                PrefabUtility.ReplacePrefab(roomGameObject, rooms[j], ReplacePrefabOptions.ReplaceNameBased);
-                Object.DestroyImmediate(roomGameObject);
             }
 
-
-            Debug.Log("nb enemies " + Selection.gameObjects[i].name + " updated: " + cpt);
+            if(enemiesRoomUpdated > 0)
+                PrefabUtility.ReplacePrefab(roomGameObject, rooms[i], ReplacePrefabOptions.ReplaceNameBased);
+            Object.DestroyImmediate(roomGameObject);
         }
+
+        Debug.Log("nb enemies updated: " + totalEnemies);
     }
 
     [MenuItem("Tools/Update enemy in rooms _F6", true)]
