867a41a5aacb12a4b64d0e247a6e0a24de97ebe1
maximelore <maxime.lore@gmail.com>
2016-11-11 18:01:14 +0100

    Modified Tiled Importer to create custom prefabs

--- Assets/Tiled2Unity/Scripts/Editor/CustomTiledImporter.cs
+++ Assets/Tiled2Unity/Scripts/Editor/CustomTiledImporter.cs
@@ -21,65 +21,44 @@ public class CustomTiledImporter : ICustomTiledImporter {
     }
 
     public void CustomizePrefab(GameObject prefab) {
-        GameObject existingPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Tiled2Unity/Prefabs/" + prefab.name + ".prefab");
+        Transform existingPrefab = AssetDatabase.LoadAssetAtPath<Transform>("Assets/Prefabs/Rooms/" + prefab.name + ".prefab");
+
         float mapWidth = prefab.GetComponent<TiledMap>().NumTilesWide;
         float mapHeight = prefab.GetComponent<TiledMap>().NumTilesHigh;
 
-        //"hack" so that the parent becomes a children
-        GameObject newTiledObj = Object.Instantiate(prefab) as GameObject;
+        GameObject roomGameObject;
+        GameObject newTiledObj = Object.Instantiate(prefab, new Vector3(prefab.transform.position.x, mapHeight, prefab.transform.position.z), Quaternion.identity) as GameObject;
         newTiledObj.name = "TiledMap";
-        Object.DestroyImmediate(prefab.GetComponent<TiledMap>());
-        for (int i = prefab.transform.childCount - 1; i >= 0; i--) {
-            Object.DestroyImmediate(prefab.transform.GetChild(i).gameObject);
-        }
-        newTiledObj.transform.parent = prefab.transform;
 
-        //Create a room component and automate the boring stuff
-        Room room = prefab.AddComponent<Room>();
         if(existingPrefab != null) {
-            room.zoneIndex = existingPrefab.GetComponent<Room>().zoneIndex;
-            room.enemiesContainers = existingPrefab.GetComponent<Room>().enemiesContainers;
+            roomGameObject = Object.Instantiate(existingPrefab.gameObject);
+
+            Object.DestroyImmediate(roomGameObject.transform.GetChild(0).gameObject); //Destroy old TiledMap
+
+            newTiledObj.transform.parent = roomGameObject.transform;
+            newTiledObj.transform.SetAsFirstSibling();
+
+            FindRoomExits(roomGameObject.GetComponent<Room>(), mapWidth, mapHeight);
+
+            PrefabUtility.ReplacePrefab(roomGameObject, existingPrefab, ReplacePrefabOptions.ReplaceNameBased);
         }
         else {
-            Debug.Log("New Room. Don't forget to add it to the dungeon!");
+            roomGameObject = new GameObject(prefab.name);
+            GameObject enemiesGameObject = new GameObject("Enemies");
+
+            Room roomComponent = roomGameObject.gameObject.AddComponent<Room>();
+            roomComponent.size = new Vector2(mapWidth / 60, mapHeight / 34); //hard coded == bad. But It's simplier. BTW future me if you had trouble because of that, I'm sory.
+            roomComponent.enemiesParent = enemiesGameObject.transform;
+
+            newTiledObj.transform.parent = roomGameObject.transform;
+            enemiesGameObject.transform.parent = roomGameObject.transform;
+
+            FindRoomExits(roomComponent, mapWidth, mapHeight);
+
+            PrefabUtility.CreatePrefab("Assets/Prefabs/Rooms/" + prefab.name + ".prefab", roomGameObject);
         }
 
-        room.size = new Vector2(mapWidth / 60, mapHeight / 34); //hard coded == bad. But It's simplier. BTW future me if you had trouble because of that, I'm sory.
-        newTiledObj.transform.position = new Vector3(newTiledObj.transform.position.x, mapHeight, newTiledObj.transform.position.z);
-
-        GameObject enemiesGameObject = existingPrefab != null ? Object.Instantiate(existingPrefab.transform.GetChild(1).gameObject) : new GameObject(); // enemies overide prevention. GetChild(1) is not a robust way, but it works.
-        enemiesGameObject.name = "Enemies";
-        enemiesGameObject.transform.parent = prefab.transform;
-        room.enemiesParent = enemiesGameObject.transform;
-        enemiesGameObject.transform.position = new Vector3(enemiesGameObject.transform.position.x, mapHeight, enemiesGameObject.transform.position.z);
-
-        //room exits search and save
-        for (int i = 0; i < colliders.Count; i++) {
-            if (CheckColliderPos(i, mapWidth, mapHeight)) {
-                continue;
-            }
-
-            for (int j = 0; j < colliders.Count; j++) {
-                if (!CheckColliderPos(j, mapWidth, mapHeight) && i != j) {
-                    if (colliders[i].width > colliders[i].height && colliders[j].width > colliders[j].height // same axis check
-                    && colliders[i].y == colliders[j].y && colliders[i].x < colliders[j].x) { // pos check
-                        Exit exit = new Exit();
-                        exit.pos = new Vector2(Mathf.Floor((colliders[i].x + colliders[i].width) / 60), colliders[i].y == 0 ? room.size.y : -1);
-                        exit.dir = new Vector2(0, colliders[i].y == 0 ? 1 : -1);
-                        room.exits.Add(exit);
-                        break;
-                    }
-                    else if (colliders[i].height > colliders[i].width && colliders[j].height > colliders[j].width
-                    && colliders[i].x == colliders[j].x && colliders[i].y < colliders[j].y) {
-                        Exit exit = new Exit();
-                        exit.pos = new Vector2(colliders[i].x == 0 ? -1 : room.size.x, -Mathf.Floor((colliders[i].y + colliders[i].height) / 34));
-                        exit.dir = new Vector2(colliders[i].x == 0 ? -1 : 1, 0);
-                        room.exits.Add(exit);
-                        break;
-                    }
-                }
-            }
-        }
+        Object.DestroyImmediate(roomGameObject);
     }
 
     public void HandleCustomProperties(GameObject layer, IDictionary<string, string> props) {
@@ -128,5 +107,36 @@ public class CustomTiledImporter : ICustomTiledImporter {
         }
 
         currentCollider.height = y - currentCollider.y;
+    }
+
+    private void FindRoomExits(Room room, float mapWidth, float mapHeight) {
+        room.exits.Clear();
+
+        for (int i = 0; i < colliders.Count; i++) {
+            if (CheckColliderPos(i, mapWidth, mapHeight)) {
+                continue;
+            }
+
+            for (int j = 0; j < colliders.Count; j++) {
+                if (!CheckColliderPos(j, mapWidth, mapHeight) && i != j) {
+                    if (colliders[i].width > colliders[i].height && colliders[j].width > colliders[j].height // same axis check
+                    && colliders[i].y == colliders[j].y && colliders[i].x < colliders[j].x) { // pos check
+                        Exit exit = new Exit();
+                        exit.pos = new Vector2(Mathf.Floor((colliders[i].x + colliders[i].width) / 60), colliders[i].y == 0 ? room.size.y : -1);
+                        exit.dir = new Vector2(0, colliders[i].y == 0 ? 1 : -1);
+                        room.exits.Add(exit);
+                        break;
+                    }
+                    else if (colliders[i].height > colliders[i].width && colliders[j].height > colliders[j].width
+                    && colliders[i].x == colliders[j].x && colliders[i].y < colliders[j].y) {
+                        Exit exit = new Exit();
+                        exit.pos = new Vector2(colliders[i].x == 0 ? -1 : room.size.x, -Mathf.Floor((colliders[i].y + colliders[i].height) / 34));
+                        exit.dir = new Vector2(colliders[i].x == 0 ? -1 : 1, 0);
+                        room.exits.Add(exit);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
