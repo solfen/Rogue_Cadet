ac911e851b0e5f62a5b58a7a9d045e6e65a18931
maximelore <maxime.lore@gmail.com>
2016-11-11 14:27:20 +0100

    EnemyUpdateInRooms multiple selection

--- Assets/Editor/EnemyUpdateInRooms.cs
+++ Assets/Editor/EnemyUpdateInRooms.cs
@@ -5,39 +5,42 @@ using UnityEditor;
 
 public class EnemyUpdateInRooms {
 
-    private static string[] roomsPaths = { "Assets/Tiled2Unity/Prefabs" };
+    private static string[] roomsPaths = { "Assets/Tiled2Unity/Prefabs", "Assets/Tiled2Unity/Prefabs/Zone1", "Assets/Tiled2Unity/Prefabs/Zone2", "Assets/Tiled2Unity/Prefabs/Zone3" };
 
     [MenuItem("Tools/Update enemy in rooms _F6")]
     private static void UpdateEnemiesRoom() {
-        string regex = "\\b" + Selection.activeGameObject.name + "\\b|\\b" + Selection.activeGameObject.name + "(Clone)" + "\\b"; //regex to match for Enemy name whole word or whole word + clone
-        List<GameObject> rooms = new List<GameObject>();
-        string[] guids = AssetDatabase.FindAssets("t:GameObject", roomsPaths);
 
-        foreach (string guid in guids) {
-            rooms.Add(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(guid)));
-        }
+        for (int i = 0; i < Selection.gameObjects.Length; i++) {
+            string regex = "\\b" + Selection.gameObjects[i].name + "\\b|\\b" + Selection.gameObjects[i].name + "(Clone)" + "\\b"; //regex to match for Enemy name whole word or whole word + clone
+            List<GameObject> rooms = new List<GameObject>();
+            string[] guids = AssetDatabase.FindAssets("t:GameObject", roomsPaths);
 
-        if (rooms.Count < 1) {
-            Debug.LogError("No rooms at " + roomsPaths[0] + ". Make sure it's the right path");
-            return;
-        }
-
-        int cpt = 0;
-        for (int i = 0; i < rooms.Count; i++) {
-            Enemy[] enemies = rooms[i].GetComponent<Room>().enemiesParent.GetComponentsInChildren<Enemy>(true);
-            for (int j = enemies.Length-1; j >= 0; j--) {
-                if (Regex.IsMatch(enemies[j].name, regex)) {
-                    Object.Instantiate(Selection.activeGameObject, enemies[j].transform.position, enemies[j].transform.rotation, enemies[j].transform.parent);
-                    Object.DestroyImmediate(enemies[j].gameObject, true);
-                    cpt++;
-                }
+            foreach (string guid in guids) {
+                rooms.Add(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(guid)));
             }
 
-            EditorUtility.SetDirty(rooms[i]);
+            if (rooms.Count < 1) {
+                Debug.LogError("No rooms at " + roomsPaths[0] + ". Make sure it's the right path");
+                return;
+            }
+
+            int cpt = 0;
+            for (int j = 0; j < rooms.Count; j++) {
+                Enemy[] enemies = rooms[j].GetComponent<Room>().enemiesParent.GetComponentsInChildren<Enemy>(true);
+                for (int k = enemies.Length - 1; k >= 0; k--) {
+                    if (Regex.IsMatch(enemies[k].name, regex)) {
+                        Object.Instantiate(Selection.gameObjects[i], enemies[k].transform.position, enemies[k].transform.rotation, enemies[k].transform.parent);
+                        Object.DestroyImmediate(enemies[k].gameObject, true);
+                        cpt++;
+                    }
+                }
+
+                EditorUtility.SetDirty(rooms[j]);
+            }
+
+
+            Debug.Log("nb enemies " + Selection.gameObjects[i].name + " updated: " + cpt);
         }
-
-
-        Debug.Log("Room enemies updated: " + cpt);
     }
 
     [MenuItem("Tools/Update enemy in rooms _F6", true)]
