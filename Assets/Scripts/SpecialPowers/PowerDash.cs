e48bdd7ceada9292390c7721d189236fa1483c93
maximelore <maxime.lore@gmail.com>
2016-11-30 16:13:47 +0100

    Power dash

---
+++ Assets/Scripts/SpecialPowers/PowerDash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerDash : BaseSpecialPower {
+
+    [SerializeField] private float speedMultiplier = 10;
+    [SerializeField] private float dashDuration = 0.25f;
+
+    private BaseMovement playerMovement;
+
+    protected override void Start() {
+        base.Start();
+        playerMovement = transform.parent.GetComponent<BaseMovement>();
+    }
+
+    protected override void Activate() {
+        StartCoroutine(Dash());
+    }
+
+    IEnumerator Dash() {
+        playerMovement.speed *= speedMultiplier;
+        yield return new WaitForSeconds(dashDuration);
+        playerMovement.speed /= speedMultiplier;
+    }
+}
