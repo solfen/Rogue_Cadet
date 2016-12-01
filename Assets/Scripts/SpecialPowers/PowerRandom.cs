5e990a224635ea9b3936c29cda42733dcfdab4da
maximelore <maxime.lore@gmail.com>
2016-12-01 17:10:48 +0100

    Power Random

---
+++ Assets/Scripts/SpecialPowers/PowerRandom.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerRandom : BaseSpecialPower {
+
+    [SerializeField] private List<BaseSpecialPower> powers;
+    private BaseSpecialPower activePower = null;
+
+    protected override void Start() {
+        base.Start();
+        activePower = Instantiate(powers[Random.Range(0, powers.Count)], transform.parent, false) as BaseSpecialPower;
+    }
+
+    protected override void Activate() {
+        StartCoroutine(WaitForDestroy());
+    }
+
+    protected override void Update() {
+        if(activePower.mana > 0) {
+            base.Update();
+        }
+    }
+
+
+    IEnumerator WaitForDestroy() {
+        yield return null;
+        while(activePower.coolDownTimer > 0) {
+            yield return null;
+        }
+
+        float currentMana = activePower.mana;
+        DestroyImmediate(activePower.gameObject);
+        activePower = Instantiate(powers[Random.Range(0, powers.Count)], transform.parent, false) as BaseSpecialPower;
+        yield return null;
+        activePower.mana = currentMana;
+        yield return null;
+        EventDispatcher.DispatchEvent(Events.SPECIAL_POWER_USED, activePower); //to activate the UI
+    }
+}
