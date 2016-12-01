6c1aa814dbfa55cfa0e901f7c7b0ce20f42a3bea
maximelore <maxime.lore@gmail.com>
2016-12-01 14:55:47 +0100

    Power fart

---
+++ Assets/Scripts/SpecialPowers/PowerFart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerFart : BaseSpecialPower {
+
+    [SerializeField] private List<AudioSource> fartNoise;
+
+    protected override void Activate() {
+        fartNoise[Random.Range(0, fartNoise.Count)].Play();
+    }
+
+}
