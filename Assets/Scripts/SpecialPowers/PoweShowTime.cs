91ca4e092f91a769a064d78ece2ddd7a0c5f557d
maximelore <maxime.lore@gmail.com>
2016-12-01 18:13:36 +0100

    Power Show time

---
+++ Assets/Scripts/SpecialPowers/PoweShowTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class PoweShowTime : BaseSpecialPower {
+
+    [SerializeField] private float duration;
+    private Text timeText;
+
+    protected override void Start() {
+        base.Start();
+        timeText = GameObject.FindGameObjectWithTag("TimeUI").GetComponent<Text>();
+    }
+    protected override void Activate() {
+        StartCoroutine(ShowTime());
+    }
+
+    IEnumerator ShowTime() {
+        float timer = duration;
+        while(timer > 0) {
+            timeText.text = System.DateTime.Now.ToString("HH:mm:ss");
+            yield return new WaitForSeconds(1);
+            timer -= 1;
+        }
+
+        timeText.text = "";
+    }
+}
