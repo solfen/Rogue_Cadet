f67a1d4e63d0413b3a7fe583117ea34889a63bd2
maximelore <maxime.lore@gmail.com>
2016-12-02 16:26:01 +0100

    GlobalData singleton

---
+++ Assets/Scripts/Utils/GlobalData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalData : MonoBehaviour {
+
+    public static GlobalData instance;
+
+    [SerializeField] private GameData _gameData;
+    public GameData gameData { get; private set; }
+    public SaveData saveData { get; private set; }
+
+	// Use this for initialization
+	void Awake () {
+        instance = this;
+        gameData = _gameData;
+        saveData = FileSaveLoad.Load();
+    }
+}
