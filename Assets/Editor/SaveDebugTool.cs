e51da51e64393528b98475b6f50f4fd070aafb62
maximelore <maxime.lore@gmail.com>
2016-12-08 12:29:17 +0100

    Tool to debug save

---
+++ Assets/Editor/SaveDebugTool.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CreateAssetMenu()]
+public class SaveDebugScriptable : ScriptableObject {
+    public SaveData data;
+}
+
+public class SaveDebugTool {
+
+    [MenuItem("Tools/Load Debug Save _F1")]
+    private static void LoadDebugSave() {
+        SaveDebugScriptable debugSave = (SaveDebugScriptable)Selection.activeObject;
+        debugSave.data = FileSaveLoad.Load();
+        Debug.Log(" ----- LOADED ------ ");
+    }
+
+    [MenuItem("Tools/Override Save With Debug _F2")]
+    private static void OverideSave() {
+        SaveDebugScriptable debugSave = (SaveDebugScriptable)Selection.activeObject;
+        FileSaveLoad.Save(debugSave.data);
+        Debug.Log("----- SAVED ------- ");
+    }
+
+    [MenuItem("Tools/Load Debug Save _F1", true)]
+    private static bool ValidateLoadDebugSave() {
+        return Selection.activeObject.GetType() == typeof(SaveDebugScriptable);
+    }
+
+    [MenuItem("Tools/Override Save With Debug _F2", true)]
+    private static bool ValidateOverideSave() {
+        return Selection.activeObject.GetType() == typeof(SaveDebugScriptable);
+    }
+}
