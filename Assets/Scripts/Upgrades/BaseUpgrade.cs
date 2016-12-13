cf0feee5ae40e6441e56020d0238d06494435c7b
maximelore <maxime.lore@gmail.com>
2016-12-13 14:01:28 +0100

    BaseUpgrade

---
+++ Assets/Scripts/Upgrades/BaseUpgrade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public abstract class BaseUpgrade : MonoBehaviour {
+
+    public string title;
+    public string description;
+    public Sprite sprite;
+    public float price;
+    public int numberOfUpgrade;
+    public float wheight;
+    public bool canUnEquip = true;
+
+    public abstract void Equip();
+    public abstract void UnEquip();
+}
