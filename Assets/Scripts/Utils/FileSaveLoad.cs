a9e7672e8cea1ac583f6cf6f586cda836c843d98
maximelore <maxime.lore@gmail.com>
2016-12-21 16:32:27 +0100

    FileSaveLoad to own file

---
+++ Assets/Scripts/Utils/FileSaveLoad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public static class FileSaveLoad {
+
+    public static void Save(SaveData data) {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameSave.dat");
+        bf.Serialize(file, data);
+        file.Close();
+
+        EventDispatcher.DispatchEvent(Events.FILE_SAVED, null);
+    }
+
+    public static SaveData Load() {
+        if (DoesSaveExists()) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameSave.dat", FileMode.Open);
+            SaveData data = (SaveData)bf.Deserialize(file);
+            file.Close();
+            return data;
+        }
+        else {
+            return new SaveData();
+        }
+    }
+
+    public static bool DoesSaveExists() {
+        return File.Exists(Application.persistentDataPath + "/gameSave.dat");
+    }
+}
--- Assets/Scripts/Utils/SaveData.cs
+++ Assets/Scripts/Utils/SaveData.cs
@@ -1,8 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
+﻿using System.Collections.Generic;
+
 
 [System.Serializable]
 public class UpgradeInfo {
@@ -38,29 +35,4 @@ public class SaveData {
     public List<ShipInfo> shipsInfo;
     public List<UpgradeInfo> upgradesInfo;
 
-}
-
-public static class FileSaveLoad {
-
-    public static void Save(SaveData data) {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/gameSave.dat");
-        bf.Serialize(file, data);
-        file.Close();
-
-        EventDispatcher.DispatchEvent(Events.FILE_SAVED, null);
-    }
-
-    public static SaveData Load() {
-        if (File.Exists(Application.persistentDataPath + "/gameSave.dat")) {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/gameSave.dat", FileMode.Open);
-            SaveData data = (SaveData)bf.Deserialize(file);
-            file.Close();
-            return data;
-        }
-        else {
-            return new SaveData();
-        }
-    }
 }
