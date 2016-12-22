ffc3422de7c2d004dcacc6ea94d76f1819e176a9
maximelore <maxime.lore@gmail.com>
2016-12-22 15:40:47 +0100

    Delete save file tool

---
+++ Assets/Editor/DeleteSaveFile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class DeleteSaveFile {
+
+    [MenuItem("Tools/Delete Save File")]
+    private static void DeleteFile() {
+        File.Delete(Application.persistentDataPath + "/gameSave.dat");
+        Debug.Log("----- DELETED ------");
+    }
+
+    [MenuItem("Tools/Delete Save File", true)]
+    private static bool DeleteFileCheck() {
+        return FileSaveLoad.DoesSaveExists();
+    }
+}
