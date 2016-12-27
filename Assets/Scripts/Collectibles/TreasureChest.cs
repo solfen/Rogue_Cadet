e894c532e9cb09f9a9a1c5cbd936373e3b0fcb47
maximelore <maxime.lore@gmail.com>
2016-12-27 15:49:54 +0100

    TreassureChest

---
+++ Assets/Scripts/Collectibles/TreasureChest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TreasureChest : MonoBehaviour {
+
+    [SerializeField] private List<GameObject> treassures = new List<GameObject>();
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D() {
+        GameObject selected = treassures[Random.Range(0, treassures.Count)];
+        selected.SetActive(true);
+        selected.transform.parent = null;
+
+        GetComponent<Rigidbody2D>().simulated = false;
+        anim.SetTrigger("Explode");
+        Destroy(gameObject, 0.6f);
+    }
+}
