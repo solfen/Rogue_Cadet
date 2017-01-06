755ac4ec39520eba7f96b11955023a3d42295986
maximelore <maxime.lore@gmail.com>
2017-01-06 16:38:13 +0100

    Achievement unlock ship

--- Assets/Scripts/Achievements/BaseAchievement.cs
+++ Assets/Scripts/Achievements/BaseAchievement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public abstract class BaseAchievement : MonoBehaviour {
 
     [SerializeField] private int achievementIndex;
+    [SerializeField] private int shipToUnlock = -1;
 
 	// Use this for initialization
 	protected virtual void Start () {
@@ -16,6 +17,9 @@ public abstract class BaseAchievement : MonoBehaviour {
     protected virtual void Unlock() {
         SaveData data = FileSaveLoad.Load();
         data.achievementsUnlocked.Add(achievementIndex);
+        if (shipToUnlock != -1)
+            data.shipsInfo[shipToUnlock].isUnlocked = true;
+
         FileSaveLoad.Save(data);
 
         Debug.Log("Achievement: " + achievementIndex + " unlcoked");
