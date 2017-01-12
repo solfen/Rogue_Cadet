529a156aa16545b484effd5d5d026788acabb675
maximelore <maxime.lore@gmail.com>
2017-01-12 16:29:45 +0100

    Damage achievements

---
+++ Assets/Scripts/Achievements/AchievementKilingSpree.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementKilingSpree : BaseAchievement {
+
+    [SerializeField] private float maxKillInterval;
+    [SerializeField] private int spreeNeededNb;
+
+    private int spreeNb;
+    private float spreeTimer = 0;
+
+    protected override void Start() {
+        base.Start();
+
+        EventDispatcher.AddEventListener(Events.ENEMY_DIED, OnEnemyDied);
+        StartCoroutine(ResetSpree());
+    }
+
+    void OnDestroy() {
+        EventDispatcher.RemoveEventListener(Events.ENEMY_DIED, OnEnemyDied);
+    }
+
+    private void OnEnemyDied(object useless) {
+        spreeTimer = 0;
+        spreeNb++;
+
+        if (spreeNb >= spreeNeededNb) {
+            Unlock();
+        }
+    }
+
+    IEnumerator ResetSpree() {
+        while(true) {
+            for(; spreeTimer < maxKillInterval; spreeTimer += Time.unscaledDeltaTime) {
+                yield return null;
+            }
+
+            spreeTimer = 0;
+            spreeNb = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Achievements/AchievementKillInLimitedTime.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementKillInLimitedTime : BaseAchievement {
+
+    [SerializeField] private float totalSeconds;
+    [SerializeField] private int killsNeededNb;
+
+    private int killsNb;
+
+    protected override void Start() {
+        base.Start();
+
+        EventDispatcher.AddEventListener(Events.ENEMY_DIED, OnEnemyDied);
+        StartCoroutine(WaitForDeadline());
+    }
+
+    void OnDestroy() {
+        EventDispatcher.RemoveEventListener(Events.ENEMY_DIED, OnEnemyDied);
+    }
+
+    private void OnEnemyDied(object useless) {
+        killsNb++;
+        if (killsNb >= killsNeededNb) {
+            Unlock();
+        }
+    }
+
+    IEnumerator WaitForDeadline() {
+        yield return new WaitForSecondsRealtime(totalSeconds);
+        Destroy(this);
+    }
+}
