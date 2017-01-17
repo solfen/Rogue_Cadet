68753bbf637e20ead7d836887aaedc156b02db74
maximelore <maxime.lore@gmail.com>
2017-01-17 17:49:15 +0100

    Cadaver rooms

---
+++ Assets/Scripts/Dungeon/CadetCadaver.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CadetCadaver : MonoBehaviour, IInteractable {
+
+    [SerializeField] private Animator dialogBox;
+    [SerializeField] private GameObject firstDialogOption;
+    [SerializeField] private List<GameObject> possibleRewards;
+
+    private Animator explosionAnim;
+
+    void Start() {
+        explosionAnim = GetComponent<Animator>();
+    }
+
+    public void Activate() {
+        Time.timeScale = 0;
+        dialogBox.SetTrigger("Open");
+        StartCoroutine(SelectUIObject());
+    }
+
+    IEnumerator SelectUIObject() {
+        yield return new WaitForSecondsRealtime(0.1f);
+        EventSystem.current.SetSelectedGameObject(firstDialogOption);
+    }
+
+    public void ConfirmAction() {
+        Time.timeScale = 1;
+        explosionAnim.SetTrigger("Explode");
+        dialogBox.SetTrigger("Close");
+
+        StartCoroutine("WaitForAnimToFinish");
+    }
+
+    IEnumerator WaitForAnimToFinish() {
+        yield return new WaitForSeconds(0.6f);
+        gameObject.SetActive(false);
+        possibleRewards[Random.Range(0, possibleRewards.Count)].SetActive(true);
+    }
+
+    public void CancelAction() {
+        Time.timeScale = 1;
+        dialogBox.SetTrigger("Close");
+    }
+}
