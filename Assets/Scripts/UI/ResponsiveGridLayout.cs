88b3e281c93227f26516c353a48bfa25a71c3ca4
maximelore <maxime.lore@gmail.com>
2017-01-17 14:28:00 +0100

    Center upgrade list

--- Assets/Scripts/UI/ResponsiveGridLayout.cs
+++ Assets/Scripts/UI/ResponsiveGridLayout.cs
@@ -20,23 +20,24 @@ public class ResponsiveGridLayout : MonoBehaviour {
         float containerArea = rectTransform.rect.width * rectTransform.rect.height;
         float maxItemSize = Mathf.Sqrt(containerArea / target.childCount); //sqrt "transforms" the area value into a square size value.
         float colNb = Mathf.Ceil(rectTransform.rect.width / maxItemSize);
-        float rowNb = Mathf.Ceil(rectTransform.rect.height / maxItemSize);
+        float rowNb = Mathf.Ceil(target.childCount / colNb);
 
         float containerRatio = rectTransform.rect.width / rectTransform.rect.height;
         float sizeScaleRatio = (maxItemSize / rectTransform.rect.width - minSize) / (maxSize - minSize); // where we are on the item size scale
         Vector2 spacing = Vector2.Lerp(minSpacing, maxSpacing, sizeScaleRatio);
         Vector2 padding = Vector2.Lerp(minPadding, maxPadding, sizeScaleRatio);
 
-        Vector2 totalSpacingSize = new Vector2((colNb - 1) * spacing.x, (rowNb - 1) * spacing.y);
+        Vector2 totalSpacingSize = new Vector2((colNb - 1) * spacing.x, (rowNb-1) * spacing.y);
         Vector2 itemSize = new Vector2((1 - padding.x * 2 - totalSpacingSize.x) / colNb, 0);
         itemSize.y = itemSize.x * containerRatio;
+        float paddingTocenterY = 1 - ( (1 - (totalSpacingSize.y + rowNb * itemSize.y)) / 2);
 
         for (int i = 0; i < target.childCount; i++) {
             Transform child = target.GetChild(i);
             float colID = i % colNb;
             float rowID = Mathf.Floor(i / colNb);
             RectTransform item = child.GetComponent<RectTransform>();
-            item.anchorMin = new Vector2(padding.x + colID * (spacing.x + itemSize.x), 1 - (padding.y + rowID * (spacing.y + itemSize.y) + itemSize.y));
+            item.anchorMin = new Vector2(padding.x + colID * (spacing.x + itemSize.x), paddingTocenterY - (rowID * (spacing.y + itemSize.y) + itemSize.y));
             item.anchorMax = new Vector2(item.anchorMin.x + itemSize.x, item.anchorMin.y + itemSize.y);
 
             if(isNavigable) {
