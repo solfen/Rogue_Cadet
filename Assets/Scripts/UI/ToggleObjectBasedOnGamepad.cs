3d30444e670a25b29d13832bedeb4eadd1c1778b
maximelore <maxime.lore@gmail.com>
2017-02-10 12:08:50 +0100

    inputUI react to change device event

--- Assets/Scripts/UI/ToggleObjectBasedOnGamepad.cs
+++ Assets/Scripts/UI/ToggleObjectBasedOnGamepad.cs
@@ -7,8 +7,16 @@ public class ToggleObjectBasedOnGamepad : MonoBehaviour {
     [SerializeField] private GameObject keyboardObject;
 
     void Start () {
+        OnInputDeviceChanged(null);
+        EventDispatcher.AddEventListener(Events.INPUT_DEVICE_CHANGED, OnInputDeviceChanged);
+    }
+
+    void OnDestroy() {
+        EventDispatcher.RemoveEventListener(Events.INPUT_DEVICE_CHANGED, OnInputDeviceChanged);
+    }
+
+    private void OnInputDeviceChanged(object useless) {
         gamepadObject.SetActive(InputManager.useGamedad);
         keyboardObject.SetActive(!InputManager.useGamedad);
     }
-
 }
