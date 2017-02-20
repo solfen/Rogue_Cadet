f46c1416da9ba343fc9207e78aeaa4778161fd4e
maximelore <maxime.lore@gmail.com>
2017-02-20 17:05:29 +0100

    Tracking AI (this time it's good)

--- Assets/Scripts/Enemies/TargetMovement.cs
+++ Assets/Scripts/Enemies/TargetMovement.cs
@@ -4,67 +4,70 @@ using System.Collections.Generic;
 
 public class TargetMovement : BaseMovement {
 
-    private enum AvoidanceDir {
-        NONE,
-        LEFT,
-        RIGHT
-    }
-
-    [Range(0,360)]
-    public float angleOffset;
+    [Range(0, 360)]
+    [SerializeField]
+    private float angleOffset;
     [Tooltip("Go toward a point at x distance before the player")]
-    public float distanceOffset;
-    public Vector3 posOffset;
-    public Transform leftEye;
-    public Transform rightEye;
-    public float eyesLength = 7.5f;
-    public float eyeAngle = 5;
-    [Tooltip("The minimum time between a dir change. To avoid left/right change at each frame")]
-    public float changeAvoidDirMinInterval = 0.3f;
-    public float laserInterval = 0.5f;
+    [SerializeField]
+    private float distanceOffset;
+    [SerializeField]
+    private Vector3 posOffset;
+    [Tooltip("distance of \"vision\"")]
+    [SerializeField]
+    private float detectionDistance = 7.5f;
+    [Tooltip("Distance form the obstacels where the enemy move perpendicularly")]
+    [SerializeField]
+    private float minObstacleDist = 1f;
+    [Tooltip("Collision detection is done with a box of the size of the collider multiplied by this")]
+    [SerializeField]
+    private float boxCastSizeMultiplier = 1.3f;
 
     private Transform playerTransform;
     private Player player;
     private Vector3 currentDir = new Vector3();
     private Quaternion offset;
-    private Quaternion leftEyeQuat;
-    private Quaternion rightEyeQuat;
     private int eyesMask = 1 << 13;
     private int playerMask = 1 << 10;
     private Dungeon dungeon;
     private GraphRoom currentRoom;
-    private KeyValuePair<AvoidanceDir, float> lastAvoidDirTime = new KeyValuePair<AvoidanceDir, float>();
-    private AvoidanceDir dominantAvoidStrategy;
+    private bool inWall = false;
+    private Vector2 colliderSize;
+    private Collider2D lastCollider;
+    private float normalPerpendicularDir;
 
-    protected override void Awake () {
+    protected override void Awake() {
         base.Awake();
         EventDispatcher.AddEventListener(Events.PLAYER_DIED, OnPlayerDeath);
 
         playerMask = playerMask | eyesMask;
-        leftEyeQuat = Quaternion.Euler(0, 0, eyeAngle);
-        rightEyeQuat = Quaternion.Euler(0, 0, -eyeAngle);
         offset = Quaternion.Euler(0, 0, angleOffset);
-        dominantAvoidStrategy = Random.value < 0.5f ? AvoidanceDir.RIGHT : AvoidanceDir.LEFT;
+        BoxCollider2D col = GetComponent<BoxCollider2D>();
+        colliderSize = col != null ? col.size * boxCastSizeMultiplier * _transform.localScale.x : Vector2.zero;
 
         GameObject go = GameObject.FindGameObjectWithTag("Player");
         if (go == null) {
             EventDispatcher.AddEventListener(Events.PLAYER_CREATED, OnPlayerCreated);
             enabled = false;
-            canSwitch = false;
+            canSwitch = false; //makes sure no external script can activate us untill initialized
             return;
-        }
-
-        GameObject dungeonGO = GameObject.FindGameObjectWithTag("Dungeon");
-        if(dungeonGO != null) {
-            dungeon = dungeonGO.GetComponent<Dungeon>();
         }
 
         Init(go.GetComponent<Player>());
     }
 
+    void OnDestroy() {
+        EventDispatcher.RemoveEventListener(Events.PLAYER_DIED, OnPlayerDeath);
+        EventDispatcher.RemoveEventListener(Events.PLAYER_CREATED, OnPlayerCreated);
+    }
+
     private void Init(Player _player) {
         playerTransform = _player.GetComponent<Transform>();
         player = _player;
+
+        GameObject go = GameObject.FindGameObjectWithTag("Dungeon");
+        if (go != null) {
+            dungeon = go.GetComponent<Dungeon>();
+        }
     }
 
     private void OnPlayerCreated(object playerObj) {
@@ -78,19 +81,8 @@ public class TargetMovement : BaseMovement {
             return;
         }
 
-        if (speed > 0) {
-            if (state)
-                StartCoroutine("LaserEyes");
-            else
-                StopCoroutine("LaserEyes");
-        }
-
         enabled = state;
-    }
-
-    void OnDestroy() {
-        EventDispatcher.RemoveEventListener(Events.PLAYER_DIED, OnPlayerDeath);
-        EventDispatcher.RemoveEventListener(Events.PLAYER_CREATED, OnPlayerCreated);
+        _rigidbody.velocity = Vector2.zero;
     }
 
     private void OnPlayerDeath(object useless) {
@@ -98,17 +90,17 @@ public class TargetMovement : BaseMovement {
         canSwitch = false;
     }
 
-	void FixedUpdate () {
+    void FixedUpdate() {
         if (!player.isInvisible) {
-            _transform.up = _transform.position - playerTransform.position; // "hack" to make it face the player // actually it's bad, it sometimes makes them move 3D...
+            LaserEyes();
+            _transform.up = _transform.position - playerTransform.position; // "hack" to make it face the player // actually it's bad, it sometimes makes them move in 3D...
             _rigidbody.velocity = currentDir * speed;
-
         }
         else {
             _rigidbody.velocity = Vector2.zero;
         }
 
-        if(dungeon != null) {
+        if (dungeon != null) {
             GraphRoom newRoom = dungeon.GetRoomFromPosition(_transform.position);
             if (newRoom != null && newRoom != currentRoom) {
                 _transform.parent = newRoom.roomInstance.enemiesParent.GetChild(0);
@@ -117,66 +109,48 @@ public class TargetMovement : BaseMovement {
         }
     }
 
-    IEnumerator LaserEyes() {
-        while (true) {
-            currentDir = playerTransform.position + posOffset - _transform.position;
-            currentDir -= currentDir.normalized * distanceOffset;
-            currentDir = offset * currentDir;
+    private void LaserEyes() {
+        currentDir = playerTransform.position + posOffset - _transform.position;
+        currentDir -= currentDir.normalized * distanceOffset;
+        currentDir = offset * currentDir;
 
-            //Debug.DrawRay(_transform.position, currentDir, Color.red);
-            //Debug.DrawRay(leftEye.position, (leftEyeQuat * currentDir).normalized * eyesLength, Color.yellow, laserInterval);
-            //Debug.DrawRay(rightEye.position, (rightEyeQuat * currentDir).normalized * eyesLength, Color.yellow, laserInterval);
+        //Debug.DrawRay(_transform.position, currentDir.normalized * detectionDistance, Color.red);
 
-            RaycastHit2D playerHit = Physics2D.Raycast(_transform.position, currentDir, eyesLength, playerMask);
-            if(playerHit.collider == null || playerHit.collider.tag != "Player") {
-                RaycastHit2D hit = Physics2D.Raycast(leftEye.position, leftEyeQuat * currentDir, eyesLength, eyesMask);
-                RaycastHit2D hit2 = Physics2D.Raycast(rightEye.position, rightEyeQuat * currentDir, eyesLength, eyesMask);
-                float minDist = hit.distance == 0 ? hit2.distance : hit2.distance == 0 ? hit.distance : Mathf.Min(hit.distance, hit2.distance);
+        RaycastHit2D playerHit = Physics2D.Raycast(_transform.position, currentDir, detectionDistance, playerMask);
+        RaycastHit2D boxCastHit = Physics2D.BoxCast(_transform.position, colliderSize, transform.rotation.eulerAngles.z, currentDir, detectionDistance, eyesMask);
+        float playerHitDist = (playerHit.collider == null || playerHit.collider.tag != "Player") ? Mathf.Infinity : playerHit.distance;
 
-                if(dominantAvoidStrategy == AvoidanceDir.RIGHT)
-                    CheckForObstacles(dominantAvoidStrategy, hit, hit2, minDist);
-                else 
-                    CheckForObstacles(dominantAvoidStrategy, hit2, hit, minDist);
+        if (boxCastHit.collider != null && boxCastHit.distance < playerHitDist) {
+            if (inWall) { // emergency reverse
+                currentDir = boxCastHit.normal;
+                inWall = boxCastHit.distance <= 0;
             }
             else {
-                ChangeAvoidDir(AvoidanceDir.NONE);
+                if (boxCastHit.collider != lastCollider) { // select avoid direction for current collider
+                    Vector3 p1 = new Vector3(-boxCastHit.normal.y, boxCastHit.normal.x, 0).normalized;
+                    Vector3 p2 = new Vector3(boxCastHit.normal.y, -boxCastHit.normal.x, 0).normalized;
+
+                    normalPerpendicularDir = (_transform.position + p1 - playerTransform.position).sqrMagnitude <= (_transform.position + p2 - playerTransform.position).sqrMagnitude ? -1 : 1;
+                    lastCollider = boxCastHit.collider;
+                }
+
+                Vector3 perpendicular = new Vector3(boxCastHit.normal.y * normalPerpendicularDir, boxCastHit.normal.x * -normalPerpendicularDir, 0).normalized;
+                currentDir = Vector3.Lerp(currentDir, perpendicular, 1 - ((boxCastHit.distance - minObstacleDist) / detectionDistance));
             }
 
-            currentDir.Normalize();
-            //Debug.DrawRay(_transform.position, currentDir * 1000, Color.blue);
-
-            yield return new WaitForSeconds(laserInterval);
+            //Debug.DrawRay(boxCastHit.point, boxCastHit.normal * 100, Color.magenta);
         }
+        else {
+            lastCollider = null;
+        }
+
+        currentDir.Normalize();
+        //Debug.DrawRay(_transform.position, currentDir * 1000, Color.blue);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Wall") {
-            currentDir = Quaternion.Euler(0, 0, 90) * currentDir;
+            inWall = true;
         }
-    }
-
-    private void ChangeAvoidDir(AvoidanceDir newDir) {
-        if(lastAvoidDirTime.Key != newDir) {
-            lastAvoidDirTime = new KeyValuePair<AvoidanceDir, float>(newDir, Time.realtimeSinceStartup);
-        }
-    }
-
-    private void CheckForObstacles(AvoidanceDir firstDir, RaycastHit2D hit1, RaycastHit2D hit2, float obstacleDist) {
-        AvoidanceDir secondDir = firstDir == AvoidanceDir.RIGHT ? AvoidanceDir.LEFT : AvoidanceDir.RIGHT;
-
-        if(hit1.collider != null && (lastAvoidDirTime.Key == firstDir || lastAvoidDirTime.Value + changeAvoidDirMinInterval < Time.realtimeSinceStartup)) {
-            AvoidCollision(firstDir, obstacleDist);
-        }
-        else if (hit2.collider != null && (lastAvoidDirTime.Key == secondDir || lastAvoidDirTime.Value + changeAvoidDirMinInterval < Time.realtimeSinceStartup)) {
-            AvoidCollision(secondDir, obstacleDist);
-        }
-        else {
-            ChangeAvoidDir(AvoidanceDir.NONE);
-        }
-    }
-
-    private void AvoidCollision(AvoidanceDir dir, float obstacleDist) {
-        currentDir = Quaternion.Euler(0, 0, Mathf.Lerp(0, 180, 1 - (obstacleDist / eyeAngle)) * (dir == AvoidanceDir.RIGHT ? -1 : 1)) * currentDir;
-        ChangeAvoidDir(dir);
     }
 }
