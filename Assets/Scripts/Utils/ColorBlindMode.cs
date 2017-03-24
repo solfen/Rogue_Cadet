3681d5200f1c80dfb12364a5e5349c6422f2d356
maximelore <maxime.lore@gmail.com>
2017-03-24 18:44:27 +0100

    Color blind mode [Uncomplete]

---
+++ Assets/Scripts/Utils/ColorBlindMode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorBlindMode : MonoBehaviour {
+
+    [SerializeField] private Color colorBlindColor;
+
+	// Use this for initialization
+	void Awake () {
+        SpriteRenderer spriteRender = GetComponent<SpriteRenderer>();
+        Graphic UIRender = GetComponent<Graphic>();
+
+        if (spriteRender == null && UIRender == null)
+            return;
+
+        if (PlayerPrefs.GetInt("ColorBlindMode", 0) == 1) {
+            if(spriteRender != null)
+                spriteRender.color = colorBlindColor;
+            else
+                UIRender.color = colorBlindColor;
+        }
+    }
+	
+}
